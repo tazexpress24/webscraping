f2e6404181654c08f643ef27b440ae89a2dc969e
Dawayne Eason <37123932+tazexpress24@users.noreply.github.com>
2018-09-14 08:52:53 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+import sys
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+
+# @app.route("/")
+# def index():
+#     mars = mongo.db.mars.find_one()
+#     print(mars, file=sys.stderr)
+
+#     return render_template("index.html", mars = mars)
+
+# @app.route("/scrape")
+# def scrape():
+#     mars = mongo.db.mars 
+#     mars_data = scrape_mars.scrape()
+#     mars.update({}, mars_data, upsert=True)
+#     return redirect("http://localhost:5000/", code=302)
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+
+
+# create mongo connection
+# client = pymongo.MongoClient()
+db = mongo.db
+#collection = db.mars_data_entries
+print("after mongodb section")
+
+@app.route("/")
+def home():
+    mars_data = db.mars.find_one()
+    return  render_template('index.html', mars_data=mars_data)
+
+@app.route("/scrape")
+def web_scrape():
+    db.collection.remove({})
+    mars_data = scrape_mars.scrape()
+    db.collection.insert_one(mars_data)
+    return  render_template('scrape.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,6 @@
+api_key = "Q9qtAUBwviacLE3NsAxA" 
+consumer_key = "3luaAvLc7TluTmeKdTr0OABMr"
+consumer_secret = "MSGvpoDB2LcgMcjTGgdRReWLyLofRLHk0UCApUlHrvGpOn5R0H"
+access_token = "1016433964096073729-YH4qAdbpIhXEeusgJSxG15CkpZa1at"
+access_token_secret = "LR04wDEOZK7hIQY2z9KWdoMvYtFOOL0uMezwh7Gx4EBXR"
+
